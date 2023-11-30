3031aca64889c95e96f93606f957ba6b188a9a21
Leonid-Vizel <spartanlaska@gmail.com>
2023-11-30 13:20:43 +0300

    pre-complete

---
+++ DbReadControlWork/Customer.cs
@@ -0,0 +1,38 @@
+﻿namespace DbReadControlWork;
+
+/// <summary>
+/// Класс описывающий продавца
+/// </summary>
+public sealed class Customer
+{
+    public int Id { get; set; }
+    public int SalesmanReference { get; set; }
+    public string Name { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public Customer() : base() { }
+
+    public static Customer? ParseRow(string initialRow)
+    {
+        var splitted = initialRow.Split('|');
+        if (splitted.Length != 4)
+        {
+            return null;
+        }
+        if (!int.TryParse(splitted[0], out int resultCol1))
+        {
+            return null;
+        }
+        if (!int.TryParse(splitted[1], out int resultCol2))
+        {
+            return null;
+        }
+        var row = new Customer()
+        {
+            Id = resultCol1,
+            SalesmanReference = resultCol2,
+            Name = splitted[2],
+            City = splitted[3]
+        };
+        return row;
+    }
+}
---
+++ DbReadControlWork/Salesman.cs
@@ -0,0 +1,33 @@
+﻿namespace DbReadControlWork;
+
+public sealed class Salesman
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public List<Customer> Customers { get; set; } = null!;
+    public Salesman() : base()
+    {
+        Customers = new List<Customer>();
+    }
+
+    public static Salesman? ParseRow(string initialRow)
+    {
+        var splitted = initialRow.Split('|');
+        if (splitted.Length != 3)
+        {
+            return null;
+        }
+        if (!int.TryParse(splitted[0], out int resultCol1))
+        {
+            return null;
+        }
+        var row = new Salesman()
+        {
+            Id = resultCol1,
+            Name = splitted[1],
+            City = splitted[2],
+        };
+        return row;
+    }
+}
